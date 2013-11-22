9d8fdedf9e80c348fe92a540a75afcb56bcfbf29
Lax <public@laxwashere.com>
2013-11-22 14:17:22 -0800

    Everyone loves broccoli

--- src/main/java/org/drtshock/Potato.java
+++ src/main/java/org/drtshock/Potato.java
@@ -17,7 +17,7 @@ public class Potato implements Tuber {
   }
 
   public boolean prepare() {
-    this.addCondiments("sour cream", "chives", "butter", "crumbled bacon", "grated cheese");
+    this.addCondiments("sour cream", "chives", "butter", "crumbled bacon", "grated cheese", "broccoli");
     return this.isDelicious();
   }
 
