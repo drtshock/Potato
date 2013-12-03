8d63e6423b6687bc9b8992f958d4771faa2c4f22
skylarjhdownes <skylarjhdownes@gmail.com>
2013-12-03 10:45:34 -0700

    Getting rid of comments before branching.
    Branching...
    
    Started making potatoes.
    
    We now have two types of potatoes.  What else should the potato types
    contain, I wonder.
    
    Potato types have been added, we have five now.  Is that enough?  The
    world may never know.
    
    Can has squashed commits?

--- src/main/java/org/drtshock/Potato.java
+++ src/main/java/org/drtshock/Potato.java
@@ -2,17 +2,20 @@ package org.drtshock;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.Color;
 
 public class Potato implements Tuber {
-
-  //Just adding a comment for experimenting with Git...
   
+  private Color skinColor;
+  private Color fleshColor;
   private final List<Condiment> condiments = new ArrayList<Condiment>();
-
+  
   public static void main(String[] args) {
-    Potato potato = new Potato();
+    AmandinePotato amadinePotato = new AmandinePotato();
+    AdirondackBluePotato adirondackBluePotato = new AdirondackBluePotato();
     GLaDOS glados = new GLaDOS();
-    if (potato.prepare()) System.out.println("Of course potato is prepared and delicious.");
+    
+    if (amadinePotato.prepare()) System.out.println("Of course potato is prepared and delicious.");
     else System.err.println("Fatal error! How could potato not be delicious?");
   }
 
@@ -26,10 +29,15 @@ public class Potato implements Tuber {
       for (String condimentName : names) condiments.add(new Condiment(condimentName));
     }
   }
+  
+  public void setColors(Color newSkinColor, Color newFleshColor) {
+    skinColor = newSkinColor;
+    fleshColor = newFleshColor;
+  }
 
   @Override
   public boolean isDelicious() {
-    return true; // obviously, potatos are always delicious
+    return true; // obviously, potatoes are always delicious
   }
 
   private class Condiment {
@@ -53,5 +61,36 @@ public class Potato implements Tuber {
       public boolean isDelicious() {
         return false; // robots are not delicious
       }
+  }
+  
+  private static class AdirondackBluePotato extends Potato {
+    private AdirondackBluePotato() {
+      setColors(Color.BLUE, Color.BLUE);
+    }
+  }
+  
+  public static class AdirondackRedPotato extends Potato{
+    public AdirondackRedPotato() {
+      setColors(Color.RED, Color.RED);
+    }
+  }
+  
+  public static class AmandinePotato extends Potato{
+    public AmandinePotato() {
+      setColors(Color.YELLOW, Color.WHITE);
+    }
+  }
+  
+  public static class RangerRussetPotato extends Potato{
+    public RangerRussetPotato() {
+      setColors(new Color(100, 75, 40), //BROWN
+              Color.WHITE);
+    }
+  }
+  
+  public static class RussetBurbankPotato extends Potato{
+    public RussetBurbankPotato() {
+      setColors(Color.YELLOW, Color.WHITE);
+    }
   }
 }
