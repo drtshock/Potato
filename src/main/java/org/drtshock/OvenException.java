d69ac87ede205f528749ecf6516eb5900c776f96
Kyle Clemens <jkc.clemens@gmail.com>
2015-10-02 12:34:34 -0400

    Updated: Formatting

--- src/main/java/org/drtshock/OvenException.java
+++ src/main/java/org/drtshock/OvenException.java
@@ -1,11 +1,8 @@
 package org.drtshock;
 
-/**
- * Created by Joe Hirschfeld on 10/2/2015.
- */
 public class OvenException extends Exception {
 
-    public OvenException(Exception internalException){
+    public OvenException(Exception internalException) {
         super(internalException);
     }
 
