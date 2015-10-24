e29313796a173bc6065bf915e7d611e9f24ec353
Laur Sisask <laursisask@gmail.com>
2015-10-24 14:05:51 +0300

    Added more constructors for exceptions.

--- src/main/java/org/drtshock/OvenException.java
+++ src/main/java/org/drtshock/OvenException.java
@@ -2,8 +2,22 @@ package org.drtshock;
 
 public class OvenException extends Exception {
 
-    public OvenException(Exception internalException) {
-        super(internalException);
+    public OvenException() {
     }
 
+    public OvenException(String message) {
+        super(message);
+    }
+
+    public OvenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OvenException(Throwable cause) {
+        super(cause);
+    }
+
+    public OvenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
