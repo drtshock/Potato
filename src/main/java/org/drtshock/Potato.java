5eec006a5a0b6237e1a3a5c98a5d12a5d495fca7
md678685 <mathew678@hotmail.co.uk>
2018-10-09 13:51:50 +0100

    Fix glaring condiment omission

--- src/main/java/org/drtshock/Potato.java
+++ src/main/java/org/drtshock/Potato.java
@@ -40,7 +40,7 @@ public class Potato implements Tuber {
      */
     public void prepare() throws NotDeliciousException {
         this.addCondiments("sour cream", "chives", "butter", "crumbled bacon", "grated cheese", "ketchup", "pepper",
-                "salt", "tabasco", "tomatoes");
+                "salt", "tabasco", "tomatoes", "onion");
         this.listCondiments();
         if (!this.isDelicious()) throw new NotDeliciousException(NotDeliciousReason.UNDERCOOKED);
     }
