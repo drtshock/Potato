38fcfb2a3a6c3b17d337f2be6ac324850dd1585c
GStudiosX <ramazanogutlu0@gmail.com>
2021-12-14 20:02:15 +0000

    remove javadocs

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.drtshock"},{"l":"org.drtshock.api"},{"l":"org.drtshock.api.condiments"},{"l":"org.drtshock.api.events"},{"l":"org.drtshock.exceptions"}];updateSearchResults();
