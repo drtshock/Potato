038294442b75872739d002853197fe3112b09dfe
Supermarcel10 <marcelbarlik@gmail.com>
2023-11-01 01:00:54 +0000

    Caught BurntException and added methods to find out how our Potato burnt

--- src/main/java/org/drtshock/Main.java
+++ src/main/java/org/drtshock/Main.java
@@ -1,5 +1,6 @@
 package org.drtshock;
 
+import org.drtshock.exceptions.BurntException;
 import org.drtshock.exceptions.NotDeliciousException;
 
 /**
@@ -15,6 +16,8 @@ public class Main {
 			System.out.println("Of course Potato is prepared and delicious.");
 		} catch (NotDeliciousException e) {
 			System.err.println("Fatal error! How could Potato not be delicious?\nReason: " + e.getReason());
+		} catch (BurntException e) {
+			System.err.println("Fatal error! Potato has been burnt to a crisp!\nTemperature: " + e.getTemperature() + " degrees");
 		}
 	}
 }
--- src/main/java/org/drtshock/exceptions/BurntException.java
+++ src/main/java/org/drtshock/exceptions/BurntException.java
@@ -5,12 +5,25 @@ package org.drtshock.exceptions;
  */
 public class BurntException extends Exception {
 
+    private int temperature;
+    private long bakeTime;
+
     public BurntException(int degrees) {
         super("Potato is badly burnt by trying to boil it at " + degrees + " degrees!!");
+        this.temperature = degrees;
     }
 
     public BurntException(long bakeTime) {
         super("Potato is badly burnt by baking for too long!! (" + bakeTime + "ms)");
+        this.bakeTime = bakeTime;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public long getBakeTime() {
+        return bakeTime;
     }
 
 }
