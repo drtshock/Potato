f30082ac4dc9d5257e4b985e296be2fd51b7f6e8
Supermarcel10 <marcelbarlik@gmail.com>
2023-11-01 00:15:26 +0000

    Implemented UndercookedException for our Potato!

---
+++ src/main/java/org/drtshock/UndercookedException.java
@@ -0,0 +1,12 @@
+package org.drtshock;
+
+/**
+ * An exception to describe that our potato isn't cooked properly!
+ */
+public class UndercookedException extends Exception {
+
+	public UndercookedException(long bakeTime) {
+		super("Potato has only been baking for " + bakeTime + "!\nGive it some more time...");
+	}
+
+}
