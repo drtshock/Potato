7d7485e139185b1f6cad4eeb3105111ea7a6d026
saylesc <cory.sayles@gmail.com>
2024-07-20 12:14:37 -0400

    Updating build and run scripts with usage

--- potato-build.sh
+++ potato-build.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 TAG=latest
 
-if [ $1 ] then
-    TAG=$1
+if ! [ -z $1 ]; then
+    if [[ $1 == "-h" || $1 == "?" ]]; then
+        # Print usage
+        echo "Usage: $0 <tag>"
+        echo "Example: $0 1.0.0"
+        exit 0
+    else
+        TAG=$1
+    fi
 fi
 
+echo "Using tag: '$TAG'"
+
 # Perform the build
-docker build -t potato:$TAG  -f Dockerfile.spud
+docker build -t potato:$TAG -f Dockerfile.spud .
--- potato-run.sh
+++ potato-run.sh
@@ -1,11 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 PREPARE_TYPE="--nonvegan"
 
-if [ $1 ]
-then
-    TAGPREPARE_TYPE=$1
+if ! [ -z $1 ]; then
+    if [[ $1 == "-h" || $1 == "?" ]]; then
+        # Print usage
+        echo "Usage: $0 <prepare type>"
+        echo "Example: $0 --vegan"
+        exit 0
+    else
+        PREPARE_TYPE=$1
+    fi
 fi
 
+echo "Running Potato with argument: \"$PREPARE_TYPE\""
+
 # Perform the build
-docker run -it --name potato-in-a-container --rm -e VEGAN=$PREPARE_TYPE potato:latest
+docker run -it --name potato-in-a-container --rm -e VEGAN=${PREPARE_TYPE} potato:latest
